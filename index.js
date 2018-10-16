cad15857d600241ee0b99bcfc135063baa43e0e1
Michael Soares <mikey.soares@gmail.com>
2018-10-16 18:15:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-// write your code below!
+
 function happyHolidays() {
-  return ("Happy holidays!")
+  return "Happy holidays!"
 }
-function happyHolidaysTo(name) {
+ function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
-function happyHolidayTo(name + holiday) {
+ function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
