187763c698ecd182eb601eb23038af5e61cf7d18
Michael Soares <mikey.soares@gmail.com>
2018-10-16 18:20:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,6 @@ function happyHolidays() {
  function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
+function holidayCountdown(holiday, days) {
+  return `It's ${days} days until ${holiday}!`
+}
