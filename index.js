9fb9316c2435135e49e901c2d45ad6509641de0c
Michael Soares <mikey.soares@gmail.com>
2018-10-16 17:50:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // write your code below!
+function happyHolidays(string) {
+  return ("Happy holidays!")
+}
+var name = "Brittany"
+function happyHolidaysTo(name) {
+  return (`"Happy holidays, ${name}!"`)
+}
